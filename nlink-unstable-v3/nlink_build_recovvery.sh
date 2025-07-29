2f2f38ddc6b0e5b39ce8b7cc78edbeccd96d9314
OBINexus TDA Agent <tda@obinexus.org>
2025-07-29 21:27:04 +0100

    W

---
+++ nlink-unstable-v3/nlink_build_recovvery.sh
@@ -0,0 +1,4 @@
+# Move the template to the expected location
+mkdir -p /home/obinexus/obinexus/projects/nlink/nlink-unstable-v3/tests/cmake/
+cp /path/to/NexusLinkConfig.cmake.in \
+   /home/obinexus/obinexus/projects/nlink/nlink-unstable-v3/tests/cmake/
